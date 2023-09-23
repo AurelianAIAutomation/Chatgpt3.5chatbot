aa691db32c896243ed9dad064865c6dc60883d1d
AurelianAIAutomation <145523362+AurelianAIAutomation@users.noreply.github.com>
2023-09-23 02:17:33 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,7 +67,7 @@
 		<h1 class="text-2xl font-bold w-full text-center">EvanBot</h1>
 		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
 	</div>
-	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
+	<div class="h-[600px] w-full bg-gray-900 rounded-md p-2 overflow-y-auto flex flex-col gap-4">
 		<div class="flex flex-col gap-2">
 			<ChatMessage type="assistant" message="Hello, ask me anything you want!" />
 			{#each chatMessages as message}
